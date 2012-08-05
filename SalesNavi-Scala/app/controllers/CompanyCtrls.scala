cae6f42bfc30d1068f3a495195d2db5269642fbb
shimamura <shimamura@basic.co.jp>
2012-08-05 22:28:44 +0900

    スケルトン追加

---
+++ SalesNavi-Scala/app/controllers/CompanyCtrls.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.data._
+import play.api.data.Forms._
+import com.codahale.jerkson.Json._
+import play.api.libs.json.JsValue
+
+/**
+ * 会社マスタ管理用のコントローラ。
+ *
+ * @author yoshiteru.shimamura
+ */
+object CompanyCtrls extends SnaviCtrls {
+	def index = Action { implicit request =>
+		Ok(views.html.company_add(null))
+	}
+}
---
+++ SalesNavi-Scala/app/controllers/UserCtrls.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.data._
+import play.api.data.Forms._
+import com.codahale.jerkson.Json._
+import play.api.libs.json.JsValue
+
+/**
+ * ユーザ管理用のコントローラ。
+ *
+ * @author yoshiteru.shimamura
+ */
+object UserCtrls extends SnaviCtrls {
+	def index = Action { implicit request =>
+		Ok(views.html.user_add(null))
+	}
+}
