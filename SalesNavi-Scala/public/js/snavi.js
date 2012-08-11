1cbc0e63cd8cea91afe07d723d281db21f53dfe3
shimamura <shimamura@basic.co.jp>
2012-08-11 20:39:31 +0900

    validationエラー時の動きを修正

--- SalesNavi-Scala/public/js/snavi.js
+++ SalesNavi-Scala/public/js/snavi.js
@@ -6,8 +6,10 @@ var snavi = {};
 (function() {
 	snavi.ajax = function(param) {
 		if (param.error  == null) {
-			param.error  = function(XMLHttpRequest, textStatus, errorThrown) {
-				alert(textStatus + ':error!!');
+			param.error  = function(response, textStatus, errorThrown) {
+				if (errorThrown == '') {
+					alert(textStatus + ":" + 通信エラー);
+				}
 			};
 		}
 		$('body').showLoading();
@@ -20,15 +22,18 @@ var snavi = {};
 			container = 'contains-body';
 		}
 		var param = {
+				container: container,
 				url: $('#' + formId).attr('action'),
 				type: 'POST',
 				data: $('#' + formId).serializeArray(),
 				dataType: 'html',
 				complete: function(response, textStatus) {
-					window.history.pushState(null, document.title, response.getResponseHeader("REDIRECT-URL"));
+					$('#' + container).html(response.responseText);
+					if (response.getResponseHeader("REDIRECT-URL") != null) {
+						window.history.pushState(null, document.title, response.getResponseHeader("REDIRECT-URL"));
+					}
 				},
 				success: function(data, dataType) {
-					$('#' + container).html(data);
 				},
 				beforeSend: function(jqXHR, settings) {
 					jqXHR.setRequestHeader('X-PJAX', 'true');
@@ -41,6 +46,8 @@ var snavi = {};
 	snavi.pjax = function(param) {
 		$.pjax(param);
 	};
+
+	$('a[data-pjax]').pjax();
 
 	snavi.lang = {};
 	snavi.lang.change = function(val) {
