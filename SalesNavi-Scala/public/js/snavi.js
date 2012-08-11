6e8c744160cda93c4529b24f50826caac334e753
shimamura <shimamura@basic.co.jp>
2012-08-11 11:18:28 +0900

    リセットを追加

--- SalesNavi-Scala/public/js/snavi.js
+++ SalesNavi-Scala/public/js/snavi.js
@@ -61,4 +61,14 @@ var snavi = {};
 		};
 		snavi.pjax(param);
 	};
+
+	snavi.reset = function(formId) {
+		$(':input', $(formId)).each(function() {
+			var type = this.type;
+			var tag = this.tagName.toLowerCase();
+			if (type == 'text' || type == 'password' || tag == 'textarea') this.value = '';
+			else if (type == 'checkbox' || type == 'radio') this.checked = false;
+			else if (tag == 'select') this.selectedIndex = -1;
+		});
+	};
 })();
